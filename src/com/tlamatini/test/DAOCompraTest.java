a2313ddfc72b8214778eb89aa368b22d7de1613c
Gabriel <cbi205322069@titlani.uam.mx>
2014-12-02 11:34:26 -0600

    DAOCompraTest corregido.

--- src/com/tlamatini/test/DAOCompraTest.java
+++ src/com/tlamatini/test/DAOCompraTest.java
@@ -15,16 +15,16 @@ import org.junit.Test;
 
 public class DAOCompraTest {
 	
-	int folio = 12;
+	int folio = 2, idUsuario = 1;
 	ArrayList<Producto> productos=new ArrayList<Producto>();
 	double importe = 150.5;
 	Date fechaOperacion= new Date(2014/02/02);
 	String nick = "admin";
 	String nombreProveedor = "emp";
 	ConexionDB conexion = new ConexionDB();
-
+    boolean iconn = conexion.crearConexion();
 	DAOCompra daoCompra = new DAOCompra(conexion);
-	Compra c = new Compra();
+	Compra c = new Compra(fechaOperacion, idUsuario);
 	
 	Date fechaInicio = new Date(2014/01/01); 
 	Date fechaFin = new Date(2015/01/01);
